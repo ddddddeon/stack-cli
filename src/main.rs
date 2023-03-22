14dd03aeb9e467ab7eea4c79d26443e3cfcfae37
ddddddeon <chris.deon@unity3d.com>
2023-03-22 12:38:42 -0400

    use filter_map + fold

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let query_string = args.join("+");
     let url = format!("https://www.google.com/search?q=site:stackoverflow.com {query_string}");
 
-    let mut links: Vec<Link> = Vec::new();
-    get(url)
+    let links = get(url)
         .and_then(|d| d.text())
         .map(|text| Document::from(text.as_str()))
         .map(|doc| {
-            doc.find(Name("a")).for_each(|el| {
-                if let Some(link) = el
-                    .attr("href")
-                    .and_then(|href| link_regex.captures(href))
-                    .and_then(|captured| captured.get(1))
-                    .map(|l| Link {
-                        name: el.text(),
-                        href: String::from(l.as_str()),
-                    })
-                {
-                    links.push(link);
-                }
-            });
+            doc.find(Name("a"))
+                .filter_map(|el| {
+                    el.attr("href")
+                        .and_then(|href| link_regex.captures(href))
+                        .and_then(|captured| captured.get(1))
+                        .map(|l| Link {
+                            name: el.text(),
+                            href: String::from(l.as_str()),
+                        })
+                })
+                .fold(Vec::new(), |mut acc, link| {
+                    acc.push(link);
+                    acc
+                })
         })?;
 
     dbg!(&links);
