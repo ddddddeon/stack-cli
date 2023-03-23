36840d41b620bbe0b33af18f0c69a1d566921284
ddddddeon <chris.deon@unity3d.com>
2023-03-23 18:09:20 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -27,18 +27,48 @@ struct JSONResponse {
     items: Vec<Answer>,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn construct_google_url() -> String {
+    let mut args: Vec<String> = args().collect();
+    args.remove(0);
+    let query_string = args.join("+");
+    return format!("https://www.google.com/search?q=site:stackoverflow.com {query_string}");
+}
+
+fn gather_answers(questions: Vec<Question>) -> Result<Vec<Answer>, Box<dyn Error>> {
     let access_token = env::var("STACKOVERFLOW_API_KEY")?;
     let key = env::var("STACKOVERFLOW_KEY")?;
+
+    for (i, question) in questions.iter().enumerate() {
+        println!("{}. {}", i, question.name);
+    }
+
+    print!("> ");
+    io::stdout().flush()?;
+
+    let mut num = String::new();
+    io::stdin().read_line(&mut num)?;
+    let num: i32 = num.trim().parse()?;
+    let question = &questions[num as usize];
+
+    let mut contents = Vec::new();
+    get(
+        format!("https://api.stackexchange.com/2.3/questions/{}/answers?site=stackoverflow&sort=activity&filter=!nOedRLr0Wi&access_token={}&key={}",
+                question.id, access_token, key))?
+        .read_to_end(&mut contents)?;
+
+    let mut decoded = String::new();
+    GzDecoder::new(&contents[..]).read_to_string(&mut decoded)?;
+
+    let answers: Vec<Answer> = serde_json::from_str::<JSONResponse>(&decoded)?.items;
+    Ok(answers)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut pp = PrettyPrinter::new();
     let link_regex =
         Regex::new(r"/url\?q=https://stack(overflow|exchange)\.com/questions/([0-9]+)/")?;
 
-    let mut args: Vec<String> = args().collect();
-    args.remove(0);
-
-    let query_string = args.join("+");
-    let url = format!("https://www.google.com/search?q=site:stackoverflow.com {query_string}");
+    let url = construct_google_url();
 
     let questions = get(url)
         .and_then(|d| d.text())
@@ -64,28 +94,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 })
         })?;
 
-    for (i, question) in questions.iter().enumerate() {
-        println!("{}. {}", i, question.name);
-    }
+    let answers = gather_answers(questions)?;
 
-    print!("> ");
-    io::stdout().flush()?;
-
-    let mut num = String::new();
-    io::stdin().read_line(&mut num)?;
-    let num: i32 = num.trim().parse()?;
-    let question = &questions[num as usize];
-
-    let mut contents = Vec::new();
-    get(
-        format!("https://api.stackexchange.com/2.3/questions/{}/answers?site=stackoverflow&sort=activity&filter=!nOedRLr0Wi&access_token={}&key={}",
-                question.id, access_token, key))?
-        .read_to_end(&mut contents)?;
-
-    let mut decoded = String::new();
-    GzDecoder::new(&contents[..]).read_to_string(&mut decoded)?;
-
-    let answers: Vec<Answer> = serde_json::from_str::<JSONResponse>(&decoded)?.items;
     answers
         .iter()
         .filter(|item| {
