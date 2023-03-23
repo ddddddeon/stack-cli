91ab3a2696d13cf5e4d9b142afab766a0e6b1291
ddddddeon <chris.deon@unity3d.com>
2023-03-23 19:42:35 -0400

    split out into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use flate2::read::GzDecoder;
+use reqwest::blocking::get;
+use serde::{Deserialize, Serialize};
+use std::{
+    env::{self, args},
+    error::Error,
+    io::{self, Read, Write},
+};
+
+#[derive(Debug)]
+pub struct Question {
+    pub name: String,
+    pub id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Answer {
+    pub body_markdown: String,
+    pub is_accepted: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct JSONResponse {
+    items: Vec<Answer>,
+}
+
+pub fn construct_google_url() -> String {
+    let mut args: Vec<String> = args().collect();
+    args.remove(0);
+    let query_string = args.join("+");
+
+    format!("https://www.google.com/search?q=site:stackoverflow.com {query_string}")
+}
+
+pub fn gather_answers(questions: Vec<Question>) -> Result<Vec<Answer>, Box<dyn Error>> {
+    let access_token = env::var("STACKOVERFLOW_API_KEY")?;
+    let key = env::var("STACKOVERFLOW_KEY")?;
+
+    for (i, question) in questions.iter().enumerate() {
+        println!("{}. {}", i, question.name);
+    }
+
+    print!("> ");
+    io::stdout().flush()?;
+
+    let mut num = String::new();
+    io::stdin().read_line(&mut num)?;
+    let num: i32 = num.trim().parse()?;
+    let question = &questions[num as usize];
+
+    let mut contents = Vec::new();
+    get(
+        format!("https://api.stackexchange.com/2.3/questions/{}/answers?site=stackoverflow&sort=activity&filter=!nOedRLr0Wi&access_token={}&key={}",
+                question.id, access_token, key))?
+        .read_to_end(&mut contents)?;
+
+    let mut decoded = String::new();
+    GzDecoder::new(&contents[..]).read_to_string(&mut decoded)?;
+    let answers: Vec<Answer> = serde_json::from_str::<JSONResponse>(&decoded)?.items;
+
+    Ok(answers)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,13 @@
 use bat::PrettyPrinter;
-use flate2::read::GzDecoder;
 use regex::Regex;
 use reqwest::blocking::get;
 use select::{document::Document, predicate::Name};
-use serde::{Deserialize, Serialize};
-use std::{
-    env::{self, args},
-    error::Error,
-    io::{self, Read, Write},
-};
-
-#[derive(Debug)]
-struct Question {
-    name: String,
-    id: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Answer {
-    body_markdown: String,
-    is_accepted: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct JSONResponse {
-    items: Vec<Answer>,
-}
-
-fn construct_google_url() -> String {
-    let mut args: Vec<String> = args().collect();
-    args.remove(0);
-    let query_string = args.join("+");
-
-    format!("https://www.google.com/search?q=site:stackoverflow.com {query_string}")
-}
-
-fn gather_answers(questions: Vec<Question>) -> Result<Vec<Answer>, Box<dyn Error>> {
-    let access_token = env::var("STACKOVERFLOW_API_KEY")?;
-    let key = env::var("STACKOVERFLOW_KEY")?;
-
-    for (i, question) in questions.iter().enumerate() {
-        println!("{}. {}", i, question.name);
-    }
-
-    print!("> ");
-    io::stdout().flush()?;
-
-    let mut num = String::new();
-    io::stdin().read_line(&mut num)?;
-    let num: i32 = num.trim().parse()?;
-    let question = &questions[num as usize];
-
-    let mut contents = Vec::new();
-    get(
-        format!("https://api.stackexchange.com/2.3/questions/{}/answers?site=stackoverflow&sort=activity&filter=!nOedRLr0Wi&access_token={}&key={}",
-                question.id, access_token, key))?
-        .read_to_end(&mut contents)?;
-
-    let mut decoded = String::new();
-    GzDecoder::new(&contents[..]).read_to_string(&mut decoded)?;
-    let answers: Vec<Answer> = serde_json::from_str::<JSONResponse>(&decoded)?.items;
-
-    Ok(answers)
-}
+use stack_cli::{construct_google_url, gather_answers, Question};
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let link_regex =
         Regex::new(r"/url\?q=https://stack(overflow|exchange)\.com/questions/([0-9]+)/")?;
-
     let url = construct_google_url();
     let questions = get(url)
         .and_then(|d| d.text())
@@ -102,7 +42,12 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
             item.is_accepted
         })
-        .map(|answer| htmlescape::decode_html(&answer.body_markdown).unwrap())
+        .map(
+            |answer| match htmlescape::decode_html(&answer.body_markdown) {
+                Ok(d) => d,
+                Err(_) => answer.body_markdown.clone(),
+            },
+        )
         .fold(Vec::new(), |mut acc, answer| {
             acc.push(answer);
             acc
